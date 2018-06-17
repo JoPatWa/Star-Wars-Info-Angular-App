e926deca6862acd1871e17b110ca0525b9965ed3
Jonathan Ward <jon.patrick.ward@gmail.com>
2018-06-16 23:54:10 -0400

    initial commit

---
+++ src/app/classes/data-movies.ts
@@ -0,0 +1,11 @@
+import { Movie } from './movie';
+
+export const MOVIES: Movie[] = [
+    {"title": "A New Hope", "episode_id":4, "opening_crawl": "nah nah bing bang", "producer": "Jon Bing"},
+    {"title": "A New Dingle", "episode_id":2, "opening_crawl": "nah nah a", "producer": "Jon Bg"},
+    {"title": "A New Beenie", "episode_id":3, "opening_crawl": "nah nah b", "producer": "Jon BArt"},
+    {"title": "A New Dog", "episode_id":1, "opening_crawl": "nah nah c", "producer": "Jon Blart"},
+    {"title": "A New Cat", "episode_id":22, "opening_crawl": "nah nah d", "producer": "Jon Boop"},
+    {"title": "A New Dragon", "episode_id":7, "opening_crawl": "nah nah e", "producer": "Jon Barp"},
+    {"title": "A New Star Trek", "episode_id":10, "opening_crawl": "nah nah f", "producer": "Jon B"}
+];
---
+++ src/app/classes/movie.ts
@@ -0,0 +1,7 @@
+export class Movie {
+
+    title: string;
+    episode_id: number;
+    opening_crawl: string;
+    producer: string;
+}
